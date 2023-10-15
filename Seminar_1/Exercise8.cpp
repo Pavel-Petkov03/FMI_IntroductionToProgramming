8ce45264d9b1189ab16db6952e72c7b54cf63dbb
Pavel Petkov <elias5247698@abv.bg>
2023-10-15 19:15:06 +0300

    doing all exercises in seminars and practice

--- Seminar_1/Exercise8.cpp
+++ Seminar_1/Exercise8.cpp
@@ -4,7 +4,6 @@ int main(){
     int numberOne, numberTwo;
     cin >> numberOne >> numberTwo;
     int result = (((numberOne - numberTwo) <= 0) * numberTwo) + (((numberTwo - numberOne) <=0) * numberOne);
-    result = (numberOne * (bool)(numberOne / numberTwo)) + (numberTwo * (bool)(numberTwo / numberOne));
     cout << result << endl;
     return 0; 
 }
